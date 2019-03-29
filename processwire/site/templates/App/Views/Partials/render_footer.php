4a7fdf924ca38bf9556b4136a1f338e65649d7d3
Scott G <scottg@creationproject.com>
2019-03-29 18:34:12 -0400

    Be social

--- processwire/site/templates/App/Views/Partials/render_footer.php
+++ processwire/site/templates/App/Views/Partials/render_footer.php
@@ -7,6 +7,19 @@
   </div>
   
   <div class="col-sm-3">
+    <div class="row">
+      <div class="col-lg-6 col-sm-12 col-xs-6 text-center line-height-normal">
+        <a href="https://twitter.com/ClinGenResource" target="_blank">
+          <i class="fab fa-twitter-square h1"></i><small class="clearfix">@ClinGenResource</small>
+        </a>
+      </div>
+      <div class="col-lg-6 col-sm-12 col-xs-6 text-center line-height-normal">
+        <a href="https://www.youtube.com/channel/UCsn4nEVUTpVQz70rClgMMsQ" class="text-danger" target="_blank">
+          <i class="fab fa-youtube-square h1"></i><small class="clearfix text-muted">youtube.com</small>
+        </a>
+      </div>
+    </div>
+    <hr />
    <?=$render_nav_footer_item['id_1017'] ?>
     <hr />
    <?=$render_nav_footer_item['id_1018'] ?>
